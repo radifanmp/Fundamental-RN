92549808519069907ef10cccddcd139429d9769f
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-12-15 11:29:57 +0700

    6.Style in React Native

--- App.js
+++ App.js
@@ -2,21 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import State from "./src/screens/state";
+import Form from "./src/screens/form";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <State />
+    <View style={{ marginTop: 200 }}>
+      <Form />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
