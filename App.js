b602d530327bb7d3493eff9de7cb38b0d4e057cd
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-12-15 09:50:56 +0700

    1.Component

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import Header from "./src/components/headers";
+import Content from "./src/components/content";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <Content />
     </View>
   );
 }
@@ -14,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/headers.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Header() {
+  return (
+    // View = Div
+    <View>
+      <Text>This is Header You know</Text>
+    </View>
+  );
+}
